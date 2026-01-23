6679fe7760925bf51df89be437276e412fa07579
souf <arhlabi.soufiane02@gmail.com>
2026-01-23 09:58:10 +0800

    export the async handler

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./logger";
 export {createEnv} from "./env";
 export type {Logger} from "pino";
-export * from "./errors/http-errors"
+export * from "./errors/http-errors";
+export * from './http/async-handler';
 export {z} from "zod";
