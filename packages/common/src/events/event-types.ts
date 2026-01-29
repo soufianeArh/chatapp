79e053a718c37b3fff72cfc66ff3df0aed1a7197
souf <arhlabi.soufiane02@gmail.com>
2026-01-29 14:17:44 +0800

    event-types decalarations + auth event

---
+++ packages/common/src/events/event-types.ts
@@ -0,0 +1,23 @@
+export type EventPayload = Record<string,unknown>;
+
+export interface DomainEvent<Ttype extends string, Tpayload extends EventPayload>{
+      type: Ttype,
+      payload: Tpayload,
+      occuredAt: Date
+};
+
+export interface EventMetadata {
+      correlatioId?: string,
+      causationId?: string,
+      version?:number
+};
+
+export interface OutboundEvent<Ttype extends string, Tpayload extends EventPayload>
+extends DomainEvent<Ttype, Tpayload>{
+      metadata?:EventMetadata
+}
+export interface InboundEvent<Ttype extends string, Tpayload extends EventPayload>
+extends DomainEvent<Ttype, Tpayload>{
+      metadata?:EventMetadata
+}
+
