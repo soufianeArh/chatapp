be8104f9c5267748accfc36c8a25cf7d33e88f3c
souf <arhlabi.soufiane02@gmail.com>
2026-03-12 11:54:40 +0800

    complete cache fro converasation

--- services/chat-service/src/cache/conversation.cache.ts
+++ services/chat-service/src/cache/conversation.cache.ts
@@ -1,6 +1,10 @@
+import { getRedisClient } from "@/clients/redis.client";
 import { Conversation } from "@/types/conversation";
 
-export const serialize = (conv: Conversation): string=>{
+const CACHE_PREFIX="converation";
+const CACHE_TTL = 60;
+
+const serialize = (conv: Conversation): string=>{
       return JSON.stringify({
             ...conv,
             createdAt: conv.createdAt.toISOString(),
@@ -8,7 +12,7 @@ export const serialize = (conv: Conversation): string=>{
       })
 }
 
-export const deserialize = (raw: string): Conversation =>{
+const deserialize = (raw: string): Conversation =>{
       const parsed =  JSON.parse(raw) as Conversation & {
             createdAt: string
             updatedAt: string
@@ -19,3 +23,24 @@ export const deserialize = (raw: string): Conversation =>{
             updatedAt: new Date(parsed.updatedAt)
       }
 }
+
+export const cacheConversationRepo = {
+      async get(conversationId: string): Promise<Conversation | null> {
+        const redis = getRedisClient();
+        const payload = await redis.get(`${CACHE_PREFIX}${conversationId}`);
+        return payload ? deserialize(payload) : null;
+      },
+      async set(converation: Conversation): Promise<void>{
+            const redis = getRedisClient();
+            await redis.setex(
+                  `${CACHE_PREFIX}${converation.id}`,
+                  CACHE_TTL,
+                  serialize(converation)
+            )
+      },
+      async delete(conversationId:string): Promise<void> {
+            const redis = getRedisClient();
+            await redis.del(`${CACHE_PREFIX}${conversationId}`);
+      }
+
+    };
